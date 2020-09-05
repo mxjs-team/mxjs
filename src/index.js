16fb5aea5e5d4d46d11c69567d897af4010f7728
Max Salles <herond12@gmail.com>
2020-09-05 16:16:30 -0300

    Added Sotorage to package index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { mix, hasMixin } from './mixin'
 import EventMixin from './event'
+import { mix, hasMixin } from './mixin'
+import { Storage } from './storage'
 
-export { mix, hasMixin, EventMixin }
+export { mix, hasMixin, EventMixin, Storage }
