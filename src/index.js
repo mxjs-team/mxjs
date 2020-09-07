9d63eb5ef7e6428965aec5725f26dd7cb2c53208
Max Salles <herond12@gmail.com>
2020-09-07 08:24:33 -0300

    Fixed Storage import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import EventMixin from './event'
 import { mix, hasMixin } from './mixin'
-import { Storage } from './storage'
+import Storage from './storage'
 
 export { mix, hasMixin, EventMixin, Storage }
